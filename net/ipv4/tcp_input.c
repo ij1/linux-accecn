024d738b791fbf7bc2b7e79f66d1403e67a4f1a0
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2020-01-17 14:48:58 +0200

    Reorder conditions

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -333,15 +333,15 @@ static u32 tcp_accecn_process(struct tcp_sock *tp, const struct sk_buff *skb,
 	u32 delta, safe_delta;
 	u32 corrected_ace;
 
+	/* Reordered ACK? (...or uncertain due to lack of data to send and ts) */
+	if (!(flag & (FLAG_FORWARD_PROGRESS|FLAG_TS_PROGRESS)))
+		return 0;
+
 	if (!(flag & FLAG_SLOWPATH)) {
 		/* AccECN counter might overflow on large ACKs */
 		if (delivered_pkts <= TCP_ACCECN_CEP_ACE_MASK)
 			return 0;
 	}
-
-	/* Reordered ACK? (...or uncertain due to lack of data to send and ts) */
-	if (!(flag & (FLAG_FORWARD_PROGRESS|FLAG_TS_PROGRESS)))
-		return 0;
 
 	/* ECT reflector in 3rd ACK (or another ACK like it), no CEP in ACE */
 	if ((tp->bytes_received == 0) && !(flag & FLAG_DATA))
