0dc6dac193c1c6042d3f1c83e8ccff1c2cee4c37
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2020-02-28 12:51:58 +0200

    Demand option also into the first data
    This will not put it into the first data if ACK for the
    reverse dir is sent first but the option goes to that ACK.

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -401,7 +401,7 @@ static void tcp_ecn_rcv_synack(struct sock *sk, const struct tcphdr *th,
 		}
 		tcp_ecn_mode_set(tp, TCP_ECN_MODE_ACCECN);
 		tp->syn_ect_rcv = ip_dsfield & INET_ECN_MASK;
-		tp->accecn_opt_demand = 1;
+		tp->accecn_opt_demand = 2;
 		if (tcp_accecn_validate_syn_feedback(sk, ace, tp->syn_ect_snt) &&
 		    INET_ECN_is_ce(ip_dsfield))
 			tp->received_ce++;
