a9239da9eada56a2d652832eb9ddaae826aa5960
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2020-03-09 14:14:35 +0200

    disconnect & option tstamp

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -2624,6 +2624,7 @@ int tcp_disconnect(struct sock *sk, int flags)
 	tp->window_clamp = 0;
 	tp->ecn_fail = 0;
 	tcp_accecn_init_counters(tp);
+	tp->accecn_opt_tstamp = 0;
 	tcp_set_ca_state(sk, TCP_CA_Open);
 	tp->is_sack_reneg = 0;
 	tcp_clear_retrans(tp);
