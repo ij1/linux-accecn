8a88e5332a97654f5e8eeebea37416cec3d317fd
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2020-03-30 16:13:35 +0300

    tcp: AccECN option beacon
    AccECN requires option to be sent a few times per RTT even
    if nothing in the ECN state requires it.
    
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -2627,6 +2627,7 @@ int tcp_disconnect(struct sock *sk, int flags)
 	tp->ecn_fail = 0;
 	tcp_accecn_init_counters(tp);
 	tp->prev_ecnfield = 0;
+	tp->accecn_opt_tstamp = 0;
 	tcp_set_ca_state(sk, TCP_CA_Open);
 	tp->is_sack_reneg = 0;
 	tcp_clear_retrans(tp);
