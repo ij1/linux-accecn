19c0a7e67a0b512f783670ecd250e9d1e3f40b3a
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2021-04-19 13:42:11 +0300

    tcp: L4S ECT(1) identifier for CC modules
    When ECN is successfully negociated for a TCP flow, it defaults to
    always use ECT(0) in the IP header. L4S service, however, needs to
    use ECT(1).
    
    This patch enables congestion control algorithms to control whether
    ECT(0) or ECT(1) should be used on a per-segment basis. A new
    CA module flag (TCP_CONG_WANTS_ECT_1) defines the behavior
    expected by the CA when not-yet initialized for the connection.
    As such, it implicitely assumes that the CA also has the
    TCP_CONG_NEEDS_ECN set.
    
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -388,6 +388,7 @@ static inline void tcp_dec_quickack_mode(struct sock *sk,
 #define	TCP_ECN_DEMAND_CWR	0x4
 #define	TCP_ECN_SEEN		0x8
 #define TCP_ECN_MODE_ACCECN	0x10
+#define TCP_ECN_ECT_1		0x20
 
 #define TCP_ECN_DISABLED	0
 #define TCP_ECN_MODE_PENDING	(TCP_ECN_MODE_RFC3168|TCP_ECN_MODE_ACCECN)
@@ -1130,8 +1131,10 @@ enum tcp_ca_ack_event_flags {
 #define TCP_CONG_NEEDS_ECN	0x2
 /* Require successfully negotiated AccECN capability */
 #define TCP_CONG_NEEDS_ACCECN	0x4
+/* Use ECT(1) instead of ECT(0) while the CA is uninitialized */
+#define TCP_CONG_WANTS_ECT_1	0x6
 #define TCP_CONG_MASK	(TCP_CONG_NON_RESTRICTED | TCP_CONG_NEEDS_ECN | \
-			 TCP_CONG_NEEDS_ACCECN)
+			 TCP_CONG_NEEDS_ACCECN | TCP_CONG_WANTS_ECT_1)
 
 union tcp_cc_info;
 
@@ -1250,6 +1253,11 @@ static inline bool tcp_ca_needs_accecn(const struct sock *sk)
 	const struct inet_connection_sock *icsk = inet_csk(sk);
 
 	return icsk->icsk_ca_ops->flags & TCP_CONG_NEEDS_ACCECN;
+}
+
+static inline bool tcp_ca_wants_ect_1(const struct sock *sk)
+{
+	return inet_csk(sk)->icsk_ca_ops->flags & TCP_CONG_WANTS_ECT_1;
 }
 
 static inline void tcp_set_ca_state(struct sock *sk, const u8 ca_state)
